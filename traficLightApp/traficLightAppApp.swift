acc284cd855343ac066d08760a70b36cf4224d99
Miller <guf302010@gmail.com>
2021-09-08 19:27:06 +0700

    Initial Commit

---
+++ traficLightApp/traficLightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  traficLightAppApp.swift
+//  traficLightApp
+//
+//  Created by Robert Miller on 08.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct traficLightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
